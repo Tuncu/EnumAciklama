d2cff07f6d1a5090ae6f86288995540e00381b87
Tunc Ulusoy <ulusoytunc@hotmail.com>
2018-07-09 22:12:09 +0300

    r

---
+++ EnumAciklama/frmMain.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace EnumAciklama
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblEnumDeger = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(39, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(167, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Seçilen Enum Değeri:";
+            // 
+            // lblEnumDeger
+            // 
+            this.lblEnumDeger.AutoSize = true;
+            this.lblEnumDeger.Location = new System.Drawing.Point(282, 55);
+            this.lblEnumDeger.Name = "lblEnumDeger";
+            this.lblEnumDeger.Size = new System.Drawing.Size(106, 13);
+            this.lblEnumDeger.TabIndex = 1;
+            this.lblEnumDeger.Text = "Seçilen Enum Değeri";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 329);
+            this.Controls.Add(this.lblEnumDeger);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblEnumDeger;
+    }
+}
+
---
+++ EnumAciklama/frmMain.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnumAciklama
+{
+
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        public static string GetEnumAciklama(Enum value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute[] attributes =
+                (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            if (attributes != null && attributes.Length > 0)
+                return attributes[0].Description;
+            else
+                return value.ToString();
+        }
+
+        public static IEnumerable<T> EnumToList<T>()
+        {
+            Type enumType = typeof(T);
+
+
+            if (enumType.BaseType != typeof(Enum))
+                throw new ArgumentException("T bir System.Enum olmalıdır");
+
+            Array enumValArray = Enum.GetValues(enumType);
+            List<T> enumValList = new List<T>(enumValArray.Length);
+
+            foreach (int val in enumValArray)
+            {
+                enumValList.Add((T)Enum.Parse(enumType, val.ToString()));
+            }
+
+            return enumValList;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            var data = EnumToList<Egitim>();
+            var comboSource = new List<EnumResult>();
+            foreach (var item in data)
+            {
+                comboSource.Add(new EnumResult
+                {
+                    Id = (int)item,
+                    Deger = GetEnumAciklama(item)
+                });
+            }
+
+            comboBox1.DataSource = comboSource;
+            comboBox1.DisplayMember = "Deger";
+            comboBox1.ValueMember = "Id";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblEnumDeger.Text = (comboBox1.SelectedItem as EnumResult).Id.ToString();
+        }
+    }
+    enum Egitim
+    {
+        [Description("İlk Okul Mezunu")]
+        IlkOkul,
+        [Description("Üniversite Mezunu")]
+        Lisans,
+        [Description("Yüksek Lisans Mezunu")]
+        YuksekLisans
+    }
+
+    class EnumResult
+    {
+        public int Id { get; set; }
+        public string Deger { get; set; }
+    }
+}
